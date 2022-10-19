4269b854838f3d25973bf84a2ab459693313117b
Dylan Liu <dylliu0209@hotmail.com>
2022-10-20 02:15:40 +1100

    add boxplot to the vis

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,6 +3,7 @@ var map = "js/hosting_countries_map.vg.json"
 var attendance = "js/attendance_linechart.vg.json"
 var barChart = "js/worldcup_winners_barchart.vg.json"
 var bubbleChart = "js/goal_stats_bubblechart.vg.json"
+var boxPlot = "js/goal_scoredperteam_boxplot.vg.json"
 
 vegaEmbed('#radialplot', radialPlot, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -21,5 +22,9 @@ vegaEmbed('#barchart', barChart, { "actions": false }).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed('#bubblechart', bubbleChart, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#boxplot', boxPlot, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
