74e63b216db57d186f3eae9cc9f5322333e830fd
Dylan Liu <dylliu0209@hotmail.com>
2022-10-19 16:01:05 +1100

    update json dimensions

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,7 +1,12 @@
-var map = "js/hosting_countries_map.vg.json";
+var radialPlot = "js/survey_radialplot.vg.json"
+var map = "js/hosting_countries_map.vg.json"
 var attendance = "js/attendance_linechart.vg.json"
 var barChart = "js/worldcup_winners_barchart.vg.json"
 var bubbleChart = "js/goal_stats_bubblechart.vg.json"
+
+vegaEmbed('#radialplot', radialPlot, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
 vegaEmbed('#host_countries_map', map, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
